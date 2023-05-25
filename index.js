13b196279c840ff06847c87d1b849e98531e2146
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-25 10:49:18 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(names, str){
+    return names.filter(function(name){
+        return (name.toUpperCase() === str.toUpperCase());
+    });
+}
+
+function fuzzyMatch(names, str){
+    return names.filter(function(name){
+        return (name[0] === str[0]);
+    });
+}
+
+function matchName(drivers, str){
+    return drivers.filter(function(driver){
+        return (driver.name === str);
+    })
+}
